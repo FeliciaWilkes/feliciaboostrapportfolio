4dc58380047f300cfc8d66a52922405e3b4266fb
FeliciaWilkes <Feliciankc@gmail.com>
2020-08-22 00:42:35 -0500

    project

--- script.js
+++ script.js
@@ -24,4 +24,13 @@ $(document).ready(function () {
         3000
         );
     });
+    // init Isotope
+let $grid = $('.grid').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    let filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
 });
